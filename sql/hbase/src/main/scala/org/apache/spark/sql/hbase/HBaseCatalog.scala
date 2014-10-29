dbee627eee9bac9df80d9df59373819c21af66b0
Bo Meng <bo.meng@huawei.com>
2014-10-29 14:16:44 -0700

    add "val" to the class definition

--- sql/hbase/src/main/scala/org/apache/spark/sql/hbase/HBaseCatalog.scala
+++ sql/hbase/src/main/scala/org/apache/spark/sql/hbase/HBaseCatalog.scala
@@ -40,10 +40,11 @@ sealed abstract class AbstractColumn(val sqlName: String, val dataType: DataType
   }
 }
 
-case class KeyColumn(sqlName: String, dataType: DataType, order: Int) extends AbstractColumn
+case class KeyColumn(val sqlName: String, val dataType: DataType, val order: Int)
+  extends AbstractColumn
 
-case class NonKeyColumn(sqlName: String, dataType: DataType, family: String, qualifier: String)
-  extends AbstractColumn {
+case class NonKeyColumn(val sqlName: String, val dataType: DataType,
+                        val family: String, val qualifier: String) extends AbstractColumn {
   @transient lazy val familyRaw = Bytes.toBytes(family)
   @transient lazy val qualifierRaw = Bytes.toBytes(qualifier)
   override def toString = {
